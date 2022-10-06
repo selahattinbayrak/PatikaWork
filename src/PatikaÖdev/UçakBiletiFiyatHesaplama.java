adcd599845cd21f9df15f13380657532bf40a953
Selahattin Bayrak <sbayrak0618@gmail.com>
2022-10-06 03:35:22 +0300

    uçak Bilet Ödevi Eklendi

--- src/PatikaÖdev/UçakBiletiFiyatHesaplama.java
+++ src/PatikaÖdev/UçakBiletiFiyatHesaplama.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class UçakBiletiFiyatHesaplama {
     public static void main(String[] args) {
         int mesafe,yas,yolculukTipi;
-        double mesafeUcret=0.10, gidisDönüşİndirimi=0,indirimliTutar = 0,toplamTutar=0,normalTutar;
+        double mesafeUcret=0.10, gidisDönüşİndirimi=0,toplamTutar=0,normalTutar;
         double yasIndirim,yasIndirim12=0.5,yasİndirim12_24=0.10,yasİndirim65=0.3, gidişDönüşInd=0.2;
         boolean isError=false;
 
@@ -23,21 +23,22 @@ public class UçakBiletiFiyatHesaplama {
             normalTutar=mesafe*mesafeUcret;
             if (yas<12) {
                 yasIndirim=normalTutar*yasIndirim12;
-                indirimliTutar=normalTutar-yasIndirim;
+                toplamTutar=normalTutar-yasIndirim;
+
             } else if (yas>=12 && yas<=24) {
                 yasIndirim=normalTutar*yasİndirim12_24;
-                indirimliTutar=normalTutar-yasIndirim;
+                toplamTutar=normalTutar-yasIndirim;
+
             } else if (yas<65 && yas>24) {
                 toplamTutar=normalTutar;
             } else if (yas>=65){
                 yasIndirim=normalTutar*yasİndirim65;
-                indirimliTutar=normalTutar-yasIndirim;
-
+                toplamTutar=normalTutar-yasIndirim;
 
             }
             if (yolculukTipi==2){
-                gidisDönüşİndirimi=indirimliTutar*gidişDönüşInd;
-                toplamTutar=(indirimliTutar-gidisDönüşİndirimi)*2;
+                gidisDönüşİndirimi=toplamTutar*gidişDönüşInd;
+                toplamTutar=(toplamTutar-gidisDönüşİndirimi)*2;
             }
 
 
