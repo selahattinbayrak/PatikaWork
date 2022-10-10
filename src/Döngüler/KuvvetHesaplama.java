c61fcb00e6eaa47d984c6211099205079f3e0ab9
Selahattin Bayrak <sbayrak0618@gmail.com>
2022-10-10 23:57:16 +0300

    4 ve 5'in kuvvetlerini hesaplama

---
+++ src/Döngüler/KuvvetHesaplama.java
@@ -0,0 +1,29 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class KuvvetHesaplama {
+
+    //Java döngüler ile girilen sayıya kadar olan 4 ve 5'in kuvvetlerini ekrana yazdıran programı yazıyoruz.
+
+    public static void main(String[] args) {
+        int n, sayac=0,sayac2=0;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Sınır Sayısını Giriniz");
+        n= scan.nextInt();
+
+        System.out.println("4'ün kuvvetleri");
+
+        for (int i = 1; i <=n; i*=4) {
+            sayac+=1;
+           System.out.println("4^"+sayac+"="+i);
+        }
+
+        System.out.println("5'in kuvvetleri");
+        for (int i = 1; i <=n; i*=5) {
+            sayac2 += 1;
+            System.out.println("5^" + sayac2 + "=" + i);
+        }
+
+    }
+}
