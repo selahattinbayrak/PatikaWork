f415b85dfde6b4c3544d1d87ad5f4d6c30b11273
Selahattin Bayrak <sbayrak0618@gmail.com>
2022-10-13 21:27:53 +0300

    Harmonik Seri Eklendi

---
+++ src/Döngüler/HarmonikSeri.java
@@ -0,0 +1,18 @@
+package Döngüler;
+
+import java.util.Scanner;
+
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+        System.out.print("n sayısını giriniz : ");
+        int n= scan.nextInt();
+        double result=0;
+        for (double i = 1; i <=n; i++) {
+            result+=(1/i);
+
+        }
+        System.out.println("Harmonik Seri ="+ result);
+
+    }
+}
